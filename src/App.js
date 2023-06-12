173f722e1a4efc1a4066348d0e548cd6d7c80cf0
Pitelom18 <pitelom18@gmail.com>
2023-06-12 17:55:04 -0500

    page 404

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Home from './pages/Home';
-import Sobre from './pages/sobre';
+import Sobre from './pages/Sobre';
+import Page404 from './pages/Page404';
 
 import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route  path='/' element={<Home />}/>
         <Route  path='/sobre' element={<Sobre />}/>
+        <Route  path='*' element={ <Page404 />}/>
       </Routes >
      </Router >
       
